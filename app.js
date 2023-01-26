c6b9e587f04acaef743b78ec3f0350e6d07855bb
vaibhav <84673505+divyavaibhav@users.noreply.github.com>
2023-01-26 20:44:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ done(null, savedUser);
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, './views'));
 app.use(express.static(path.join(__dirname, './public')));
-app.use('/images', express.static('./images'));
+app.use('/images', express.static(path.join(__dirname, './images')));
 
 app.use(session({
 secret: "IAMDUMBLIKEYOUNOOBX100LOLXDHAHAAH#5642540780BNIJIPSRJBIRSJIJ",
